9489ebd6fb4a2bfc498dd17f29ccf1399a155cdc
Ram Prasad <rampgogu@in.ibm.com>
2018-08-27 16:52:51 +0530

    Removing Blockchain code

--- src/main/java/com/nestle/document/dao/LoginDao.java
+++
@@ -1,9 +0,0 @@
-package com.nestle.document.dao;
-
-public interface LoginDao {
-
-	public boolean validateLogin(String username, String password);
-
-	public String getRoleByUserName(String userName);
-
-}
